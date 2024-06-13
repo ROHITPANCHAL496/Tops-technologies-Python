64320fea7dc281d240c7140683ab8deba5118da0
ROHITPANCHAL496 <rohitpanchal496@gmail.com>
2024-06-13 11:26:55 +0530

    fundamentals of python

---
+++ ASSIGNMENT/MODULE 1/Q1.py
@@ -0,0 +1,3 @@
+# Q.    What is Software?
+
+# Ans.  Software is sets of programme and instructions which perform sepecific task when execute.
---
+++ DAY 1 INTRO.py
@@ -0,0 +1,36 @@
+#TOKENS
+# 1. Identifiers 2. Variables 3. Keywords 4.datatype
+#x=20
+#x+=20
+#print(x)
+
+#X=10
+#print(type(X))
+#name="rohit"
+#print(type(name))
+#name=["Apple","Banana", "Tamato", "Patato"]
+#print(type(name))
+
+#name={"Apple","Banana", "Tamato", "Patato"}
+#print(type(name))
+#Arithmetic operators
+#Assignment operators
+#+ Add,
+#- substraction,
+#* multiplication,
+#/ division,
+#** exponential
+#X=5
+#Y=10
+#print(X+Y)
+#X=5
+#Y=10
+#print(X-Y)
+
+#X=10
+#Y=5
+#print(X/Y)
+X=10
+Y=5
+print(X%Y)
+
